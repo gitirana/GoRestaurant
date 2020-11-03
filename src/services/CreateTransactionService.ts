f1d869ce719f5aac3c3590f5bf108cd22b518111
gitirana <tlgc@poli.br>
2020-11-03 00:27:33 -0300

    finalizando desafio1 node nivel 2

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,11 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome'
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +13,20 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if(type === 'outcome' && total < value) {
+      throw new Error('You dont have enough balance.');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type
+    })
+
+    return transaction;
   }
 }
 
